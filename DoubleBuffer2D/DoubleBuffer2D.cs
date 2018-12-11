c4201febec60db38d9fe91ce81769eee3028b81b
fs000 <flaviosantos002010@hotmail.com>
2018-12-11 16:38:46 +0000

    Terminado o Exercício 2

---
+++ DoubleBuffer2D/DoubleBuffer2D.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleBuffer2D {
+    class DoubleBuffer2D<T> {
+
+        private T[,] current;
+
+        private T[,] next;
+
+        private int XDim { get; }
+
+        private int YDim { get; }
+
+        public DoubleBuffer2D(int x, int y) {
+
+            XDim = x;
+            YDim = y;
+            current = new T[XDim, YDim];
+            next = new T[XDim, YDim];
+            Clear();
+        }
+
+        public T this[int x, int y] {
+
+            get {
+
+                return current[x, y];
+            }
+
+            set {
+
+                next[x, y] = value;
+            }
+        }
+
+        public void Clear() {
+
+            Array.Clear(next, 0, next.Length);
+        }
+
+        public void Swap() {
+
+            T[,] aux = current;
+            current = next;
+            next = aux;
+        }
+    }
+}
---
+++ DoubleBuffer2D/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleBuffer2D {
+    class Program {
+        static void Main(string[] args) {
+
+            DoubleBuffer2D<char> db = new DoubleBuffer2D<char>(5, 5);
+
+            db[0, 0] = 'a';
+            db[0, 1] = 'b';
+            db[0, 2] = 'c';
+            db[0, 3] = 'd';
+            db[0, 4] = 'e';
+            db[1, 0] = 'a';
+            db[1, 1] = 'b';
+            db[4, 2] = 'X';
+
+            db.Swap();
+
+            Console.WriteLine($"{db[0, 0]}");
+        }
+    }
+}
