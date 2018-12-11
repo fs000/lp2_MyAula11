844effdc04a634a274f07b5dde23ee582fd74dbc
fs000 <flaviosantos002010@hotmail.com>
2018-12-11 15:14:59 +0000

    Terminado o Exercício 1

---
+++ Indexers/MyVector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers {
+    struct MyVector {
+
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public MyVector(int x, int y) {
+
+            X = x;
+            Y = y;
+        }
+
+        public float this[int index] {
+
+            get {
+
+                if (index == 0) { return X; }
+                else if (index == 1) { return Y; }
+                else { throw new IndexOutOfRangeException(); }
+            }
+
+            set {
+
+                if (index == 0) { X = value; }
+                else if (index == 1) { Y = value; }
+            }
+        }
+
+        public float this[string s] {
+
+            get {
+
+                s = s.ToLower();
+                if (s == "x" || s == "a" || s == "0") { return X; }
+                else if (s == "y" || s == "b" || s == "1") { return Y; }
+                else { throw new IndexOutOfRangeException(); }
+            }
+
+            set {
+                
+                if (s == "x" || s == "a" || s == "0") { X = value; }
+                else if (s == "y" || s == "b" || s == "1") { Y = value; }
+            }
+        }
+    }
+}
--- Indexers/Program.cs
+++ Indexers/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace Indexers {
     class Program {
         static void Main(string[] args) {
+
+            int x = 800;
+            int y = 600;
+
+            MyVector mv = new MyVector(x, y);
+
+            Console.WriteLine(mv[0]);
+            Console.WriteLine(mv[1]);
+            Console.WriteLine(mv["y"]);
+            Console.WriteLine(mv["a"]);
+            Console.WriteLine(mv["B"]);
         }
     }
 }
